36e02b6069813a8f31c361360e5b1b65cc224e9c
John Howlett <johnhowlett@gmx.ch>
2019-09-06 16:44:46 +0200

    Module Creating React Components

---
+++ src/components/AboutPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class AboutPage extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h2>About</h2>
+        <p>This app users Reace.</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default AboutPage;
